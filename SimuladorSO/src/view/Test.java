f6d7bcb98e200ca1d419a5af85c62dbef85a5637
zaxa85 <zaxa85@f9d161e4-4b95-11de-8f29-b90405f7cc0d>
2009-05-28 14:54:20 +0000

    Para eliminar

---
+++ SimuladorSO/src/view/Test.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package view;
+
+/**
+ *
+ * @author pablo.sierralta
+ */
+public class Test {
+
+}
